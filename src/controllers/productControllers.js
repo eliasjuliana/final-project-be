9aa59fe0032bafb5b665cfdd356b9320233240e6
Juliana Elias <eliasjuliana91@gmail.com>
2023-11-29 17:56:08 -0300

    agrego endpoint post de productos

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,15 +1,53 @@
 import ProductModel from '../models/productSchema.js';
 
 export const getProducts = async (_, res) => {
-    try {
-        const data = await ProductModel.find({});
+  try {
+    const data = await ProductModel.find({});
 
-        res.json(data);
-    } catch (e) {
-        console.error(e);
+    res.json(data);
+  } catch (e) {
+    console.error(e);
 
-        res.status(500).json({
-            message: 'An error occured while connecting to the DB',
-        });
+    res.status(500).json({
+      message: 'An error occured while connecting to the DB',
+    });
+  }
+};
+
+export const postProducts = async (req, res) => {
+  const { body } = req;
+
+  /* traigo BODY del FrontEnd */
+
+  const newProduct = new ProductModel({
+    name: body.name,
+    image: body.image,
+    price: body.price,
+    description: body.description,
+    amount: body.amount,
+    isAvailable: true,
+    isOrdered: false,
+  });
+
+  try {
+    await newProduct.save();
+
+    res.status(201).json({
+      data: null,
+      message: 'Product added succesfully',
+    });
+  } catch (e) {
+    if (e.message.includes('duplicate')) {
+      res.status(400).json({
+        data: null,
+        message: 'The product name is already used',
+      });
+      return;
     }
-    };
+
+    res.status(500).json({
+      data: null,
+      message: 'An error occured while posting the product',
+    });
+  }
+};
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import { getProducts } from '../controllers/productControllers.js';
+import { getProducts, postProducts } from '../controllers/productControllers.js';
 
 const router = express.Router();
 
+// GET
 router.get('/', getProducts);
+
+// POST
+router.post('/', postProducts);
 
 export default router;
